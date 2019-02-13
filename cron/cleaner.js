d9e6025ba519c2c641cb9826e85833fcc0e7d393
Pratik Lunagaria <pratiklunagaria@hotmail.com>
2019-02-13 10:11:04 +0530

    removed unnecessary files

--- cron/cleaner.js
+++
@@ -1,7 +0,0 @@
-const dbcleaner = require('knex-cleaner');
-const db = require('../config/coinsdb');
-
-dbcleaner.clean(db).then(function() {
-    // your database is now clean
-    console.log('cleaned');
-  });
