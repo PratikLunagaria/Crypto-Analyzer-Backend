e0d2ddabe1f1b37cc53207f3b8e895872c9173c5
Pratik Lunagaria <pratiklunagaria@hotmail.com>
2019-02-21 10:54:50 +0530

    removed unnecessary packages

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 //TODO: enable this in production
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "https://condescending-kowalevski-f5bd7e.netlify.com");
+  res.header('Access-Control-Allow-Origin', "http://localhost:3000");
   res.header('Access-Control-Allow-Methods','GET');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
